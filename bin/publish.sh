08ae4c27ab85751b3ccfda1a2869f19895cffc46
Hunor <hunor@unumotors.com>
2020-04-05 22:22:10 +0200

    Update build

--- bin/publish.sh
+++
@@ -1,8 +0,0 @@
-npm run build
-git add dist
-git commit -m "Update build"
-git subtree split --prefix dist -b gh-pages
-git push -f origin gh-pages:gh-pages
-git branch -D gh-pages
-rm -r dist
-echo "Published"
