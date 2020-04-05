7dc489eafae37083e3fafa8c7dd9c52a53cfb61c
Hunor <hunor@unumotors.com>
2020-04-05 19:54:21 +0200

    Update build

---
+++ dist/precache-manifest.67873d304c147e187c751a1ea17b0ae7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de8512c7acd07b81b6b3",
+    "url": "compass/css/app.cb61e048.css"
+  },
+  {
+    "revision": "e50914bccb6f7789d495d75868ae30ae",
+    "url": "compass/index.html"
+  },
+  {
+    "revision": "de8512c7acd07b81b6b3",
+    "url": "compass/js/app.bfb8f1e3.js"
+  },
+  {
+    "revision": "b4c5e6db501a2d68a374",
+    "url": "compass/js/chunk-vendors.1070181b.js"
+  },
+  {
+    "revision": "cd4fdca547c522dd59ff4b079c09c515",
+    "url": "compass/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "compass/robots.txt"
+  }
+]);
