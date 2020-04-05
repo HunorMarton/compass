2f900e59cc0eff467693bb5ac05de0860aa67fc4
Hunor <hunor@unumotors.com>
2020-04-05 22:22:10 +0200

    Update build

---
+++ dist/precache-manifest.0dc145c1c8c6f7ee9a721513ee6665ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1a22ba6be98e6e08970",
+    "url": "css/app.cb61e048.css"
+  },
+  {
+    "revision": "55505ed271a3780ca7b4b69c206b3653",
+    "url": "index.html"
+  },
+  {
+    "revision": "c1a22ba6be98e6e08970",
+    "url": "js/app.8afd9c30.js"
+  },
+  {
+    "revision": "b4c5e6db501a2d68a374",
+    "url": "js/chunk-vendors.1070181b.js"
+  },
+  {
+    "revision": "cd4fdca547c522dd59ff4b079c09c515",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
